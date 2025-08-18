5e3d9d0d0184c1412c69e3735c99ddb809fdb2d3
EmilioJoseRomero <emilio.romero@ucr.ac.cr>
2025-08-18 14:34:52 -0600

    Creación del proyecto y solucion del laboratorio 1

---
+++ Laboratorio1_B96946/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_B96946.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
