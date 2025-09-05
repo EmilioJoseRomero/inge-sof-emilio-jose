99b30792fca01aee2690c81a37ba76703daf0ba6
EmilioJoseRomero <emilio.romero@ucr.ac.cr>
2025-09-04 20:14:25 -0600

    Se sube el lab numero 4

---
+++ backend-lab/Models/CountryModel.cs
@@ -0,0 +1,11 @@
+﻿namespace backend_lab.Models
+{
+    public class CountryModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Continent { get; set; }
+        public string Languaje { get; set; }
+      
+    }
+}
---
+++ backend-lab/Services/CountryService.cs
@@ -0,0 +1,22 @@
+﻿using backend_lab.Handlers.backend_lab.Repositories;
+using backend_lab.Models;
+using backend_lab.Services;
+
+namespace backend_lab.Services
+{
+    public class CountryService
+    {
+        private readonly CountryRepository countryRepository;
+        public CountryService()
+        {
+            countryRepository = new CountryRepository();
+        }
+
+        public List<CountryModel> GetCountries()
+        {
+            // Add any missing business logic when it is neccesary
+            return countryRepository.GetCountries();
+        }
+
+    }
+}
